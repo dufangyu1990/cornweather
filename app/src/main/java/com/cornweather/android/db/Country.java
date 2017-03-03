cd1cbb5a1bf2a2723637fb745150702733f8b1e5
dufangyu <781675707@qq.com>
2017-03-03 16:38:07 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/cornweather/android/db/Country.java
@@ -0,0 +1,47 @@
+package com.cornweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by dufangyu on 2017/3/3.
+ * 县
+ */
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private int cityId;
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
